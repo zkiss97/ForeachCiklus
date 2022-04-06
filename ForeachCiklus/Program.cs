d8a0b38b622d4e31b6f7dd92140b461d5503bfcb
zkiss97 <zkiss1997@gmail.com>
2022-04-06 10:50:22 +0200

    kész

--- ForeachCiklus/Program.cs
+++ ForeachCiklus/Program.cs
@@ -97,6 +97,83 @@ namespace ForeachCiklus
 
     }
 
+    class ForAndEach
+    {
+        private int a;
+        private int[] fortomb = new int[10];
+        private int[] foreachtomb = new int[10];
+        Random rnd = new Random();
+
+        public ForAndEach(int a)
+        {
+            this.a = a;
+        }
+
+        public ForAndEach()
+        {
+            this.a = 0;
+        }
+
+        public void Forciklus()
+        {
+            Console.WriteLine("Alább látható a sima for ciklus eredménye: ");
+            for (int i = 0; i < 10; i++)
+            {
+                fortomb[i] = rnd.Next(0, 100);
+                Console.WriteLine(fortomb[i]);
+            }
+        }
+
+        public void ForeachCiklus()
+        {
+            Console.WriteLine("\nAlább látható a foreach ciklus eredménye: ");
+            for (int i = 0; i < 10; i++)
+            {
+                foreachtomb[i] = rnd.Next(0, 100);
+            }
+
+            foreach (int i in foreachtomb)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+
+    class Fibonacci
+    {
+        private int eddig;
+        private int number;
+
+        public Fibonacci(int eddig)
+        {
+            this.eddig = eddig;
+            this.number = number;
+        }
+
+        public Fibonacci()
+        {
+            this.eddig = 0;
+            this.number = 2;
+        }
+
+        public void Fibnumbers()
+        {
+            Console.WriteLine("Add meg hanyadik elemig írjam ki a Fibonacci számokat:");
+            eddig = int.Parse(Console.ReadLine());
+
+            int a = 0, b = 1, c = 0;
+            Console.WriteLine("{0}, {1}", a, b);
+            for (int i = 2; i <= eddig; i++)
+            {
+                c = a + b;
+                Console.WriteLine("{0}", c);
+                a = b;
+                b = c;
+            }
+
+        }
+
+    }
 
 internal class Program
     {
@@ -105,8 +182,15 @@ internal class Program
             //Listak l1 = new Listak();
             //l1.Feltolt();
 
-            Elagazas e1 = new Elagazas();
-            e1.Feltolt();
+            //Elagazas e1 = new Elagazas();
+            //e1.Feltolt();
+
+            //ForAndEach fae1 = new ForAndEach();
+            //fae1.Forciklus();
+            //fae1.ForeachCiklus();
+
+            Fibonacci fib1 = new Fibonacci();
+            fib1.Fibnumbers();
 
             Console.ReadLine();
         }
